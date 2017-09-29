6567fb81fee442a6dc05bb2a9ccd542e6e512676
mahdi naser <mahdinaser.yahoo.com>
2017-09-28 22:03:08 -0500

    more clean up

--- HW2.R
+++ HW2.R
@@ -1,15 +1,17 @@
 library(microbenchmark)
-## a class exmaple that have 2 courses
-## using the tapply we calulate the mean grade for the class
-## tapply basically group each class and then 
-## calcuate the mean grade for each group
+## Craete the data frame for courses and grades
 class.exmple <-
   data.frame(student = 1:100,
              grade = rnorm(100, mean = 60, sd = 12),
              class = gl(2, 50,labels = c("DataAnalysis", "OS")))
 
+
+## Run the tapply to calculate the mean for each course
+
 tapply(class.exmple$grade, class.exmple$class, mean)
 
+
+## Create a function to simulate the tapply
 
 tapplyUsingFunction <- function(dt) {
 summationT = 0;
@@ -23,23 +25,25 @@ for (i in seq_along(dt$grade)) {
     summationC <- dt$grade[i]+summationC
     }
 }
-#print(summationT/50);
-#print(summationC/50);
+print(summationT/50);
+print(summationC/50);
 }
 tapplyUsingFunction(class.exmple)
 
-microbenchmark(tapplyUsingFunction(class.exmple),tapply(class.exmple$grade, class.exmple$class, mean))
+# Compare using the microbenchmark 
+momTotal = microbenchmark(tapplyUsingFunction(class.exmple),tapply(class.exmple$grade, class.exmple$class, mean))
+autoplot(momTotal)
 
-
-#mapply gives us a way to call a non-vectorized function in a vectorized way.
 
 mapply(rep,1:4,4:1)
 
-mapplyUsingFunction <- function(){
+mapplyUsingFunction <- function(n){
 outputList={}
-for (i in 1:4) {
-  outputList<-append(rep(i,4-i+1),outputList)
+for (i in 1:n) {
+  outputList<-append(rep(i,n-i+1),outputList)
 }
 outputList
 }
-time<-microbenchmark(mapplyUsingFunction(),mapply(rep,1:4,4:1))
+
+momTotal = microbenchmark(mapplyUsingFunction(bench),mapply(rep,1:bench,bench:1))
+autoplot(momTotal)
